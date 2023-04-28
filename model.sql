1e52232d5757e1a7fc095bade76f4d5a4d44bab6
Bahodir_Ziyodaliyev <113345966+BahodirZIyodali@users.noreply.github.com>
2023-04-28 18:53:58 +0500

    Update model.sql

--- model.sql
+++ model.sql
@@ -1,16 +1,10 @@
 
-\l
-\c nt
-\d
-\dt
-
 
 CREATE TABLE Users (
   user_id INT PRIMARY KEY,
   user_name VARCHAR(50),
   user_email VARCHAR(50)
 );
-
 
 CREATE TABLE Room (
   room_id INT PRIMARY KEY,
@@ -24,12 +18,10 @@ CREATE TABLE Groups (
   FOREIGN KEY (room_id) REFERENCES Room(room_id)
 );
 
-
 CREATE TABLE Course (
   course_id INT PRIMARY KEY,
   course_name VARCHAR(50)
 );
-
 
 CREATE TABLE UserCourse (
   user_id INT,
@@ -39,22 +31,22 @@ CREATE TABLE UserCourse (
   FOREIGN KEY (course_id) REFERENCES Course(course_id)
 );
 
-SELECT * from UserCourse;
-
-INSERT INTO Users (user_id, user_name, user_email) VALUES (1, 'John Doe', 'johndoe@example.com');
-
-INSERT INTO Groups (group_id, group_name, room_id) VALUES (1, 'Group N74', 123);
+INSERT INTO Users (user_id, user_name, user_email) VALUES (1, 'Eshmat ', 'TOSHMATOV@example.com');
 
 INSERT INTO Course (course_id, course_name) VALUES (1, 'Full stack ');
+
+  INSERT INTO Groups (group_id, group_name, room_id) VALUES (1, 'Group N74', 1);
 
 INSERT INTO UserCourse (user_id, course_id) VALUES (1, 1);
 
 
 
-INSERT INTO Users (user_id, user_name, user_email) VALUES (2, 'Johna Doe', 'johnadoe@example.com');
 
-INSERT INTO Course (course_id, course_name) VALUES (2, 'Design ');
 
-INSERT INTO Groups (group_id, group_name, room_id) VALUES (2, 'Group N20', 3);
+INSERT INTO Users (user_id, user_name, user_email) VALUES (2, 'John Doe', 'johndoe@example.com');
+
+INSERT INTO Course (course_id, course_name) VALUES (2, 'Design');
+
+INSERT INTO Groups (group_id, group_name, room_id) VALUES (2, 'Group N20', 2);
 
 INSERT INTO UserCourse (user_id, course_id) VALUES (2, 2);
